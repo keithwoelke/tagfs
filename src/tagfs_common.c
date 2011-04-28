3c9d17d50738812e7e0e14ab9beee63c59e8f61e
williamwoelke <williamwoelke@gmail.com>
2011-04-27 22:22:29 -0700

    foo

--- src/tagfs_common.c
+++ src/tagfs_common.c
@@ -69,6 +69,75 @@ char *file_name_from_id(int file_id) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int path_to_array(const char *path, char ***array) {
+	DEBUG(ENTRY);
+	char *tmp_path = NULL;
+	char *tok_ptr = NULL;
+	char *token = NULL;
+	int i = 0;
+	int num_tokens = 0;
+
+	assert(path != NULL);
+	assert(array != NULL);
+	assert(*array == NULL);
+
+	DEBUG("Converting %s to array", path);
+
+	if(strcmp(path, "/") != 0) {
+		/* calculate the number of tags in the path */
+		num_tokens = num_tags_in_path(path);
+		DEBUG("Number of tokens in path: %d", num_tokens);
+
+		/* make a copy of the path that can be modified */
+		tmp_path = malloc(strlen(path) * sizeof(*tmp_path) + 1);
+		assert(tmp_path != NULL);
+
+		*array = malloc(num_tokens * sizeof(**array));
+		assert(*array != NULL);
+
+		strcpy(tmp_path, path);
+		assert(strcmp(path, tmp_path) == 0);
+
+		/* copy tags from path into an array */
+		token = strtok_r(tmp_path, "/", &tok_ptr);
+		DEBUG("Array contents:");
+		for(i = 0; token != NULL; i++) {
+			(*array)[i] = malloc(strlen(token) * sizeof((**array)[i]) + 1);
+			assert((*array)[i] != NULL);
+			strncpy((*array)[i], token, strlen(token) + 1);
+			assert(strcmp((*array)[i], token) == 0);
+			DEBUG("array[%d] = %s, at address %p.", i, (*array)[i], (*array)[i]);
+			token = strtok_r(NULL, "/", &tok_ptr);
+		}
+
+		assert(tmp_path);
+		free(tmp_path);
+		tmp_path = NULL;
+
+		DEBUG("Returning array from %s with %d element(s).", path, num_tokens);
+	}
+
+	DEBUG(EXIT);
+	return num_tokens;
+} /* path_to_array */
+
 static bool unique_tags_in_path(const char *path) {
 //	bool unique = true;
 	char **tag_array = NULL;
