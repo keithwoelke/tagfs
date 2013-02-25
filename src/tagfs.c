81c80421f083db1215a45fcc90306864da1f398f
Keith Woelke <keith@keith-desktop.(none)>
2013-02-24 21:55:31 -0800

    Fixed a minor bug with displaying folders, but no files at root.

--- src/tagfs.c
+++ src/tagfs.c
@@ -416,7 +416,7 @@ int tagfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t off
 	}
 
 	/* if there are files at the requested location, or we are at root, show folders */
-	if(num_files > 0) {
+	if(num_files > 0 || strcmp("/", path) == 0) {
 		/* add folders */
 		num_folders = folders_at_location(path, files, num_files, &folders);
 
